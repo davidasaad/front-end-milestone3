ae907c49d69fbf71bfdfec5f0c9dd9754a6a7cb4
David <dsa38@njit.edu>
2025-02-20 13:38:08 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import Film from './Film';
+import Customer from './Customer';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/films" element={<Film />} />
+      <Route path="/customers" element={<Customer />} />
+    </Routes>
+  </Router>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
